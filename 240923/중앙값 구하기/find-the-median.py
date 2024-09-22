6f7c968cfe082331a64d70e1857d66e02ffcdf6d
Seokgyu Choi <100775619+chosey0@users.noreply.github.com>
2024-09-23 05:13:53 +0900

    Title: 중앙값 구하기, Time: 396ms, Memory: 32MB, Status: Passed - CodeTree

--- 240923/중앙값 구하기/find-the-median.py
+++ 240923/중앙값 구하기/find-the-median.py
@@ -1,6 +1,5 @@
 import sys
 import statistics as st
 
-abc = [int(val) for val in sys.stdin.readline().split(" ")]
-
-print(st.median(abc))
+abc = sorted([int(val) for val in sys.stdin.readline().split(" ")])
+print(abc[len(abc)//2]) if len(abc) % 2 == 0 else print(abc[(len(abc) - 1) // 2])
