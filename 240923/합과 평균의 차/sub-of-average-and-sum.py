88238eeb877b285ca5ddaa2d2a980ea263525508
Seokgyu Choi <100775619+chosey0@users.noreply.github.com>
2024-09-23 04:45:18 +0900

    Title: 합과 평균의 차, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/합과 평균의 차/sub-of-average-and-sum.py
+++ 240923/합과 평균의 차/sub-of-average-and-sum.py
@@ -1,5 +1,7 @@
 import sys
-import statistics as st
 
 vals = [int(val) for val in sys.stdin.readline().split(" ")]
-print(f"{sum(vals)}\n{st.mean(vals)}\n{sum(vals)-st.mean(vals)}")
+v_sum = sum(vals)
+v_mean = int(v_sum/len(vals))
+
+print(f"{v_sum}\n{v_mean}\n{v_sum-v_mean}")
