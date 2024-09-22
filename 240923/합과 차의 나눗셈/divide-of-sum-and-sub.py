3a3c88e615e1439184f04e8ba3346f99e55e6601
Seokgyu Choi <100775619+chosey0@users.noreply.github.com>
2024-09-23 04:39:16 +0900

    Title: 합과 차의 나눗셈, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/합과 차의 나눗셈/divide-of-sum-and-sub.py
+++ 240923/합과 차의 나눗셈/divide-of-sum-and-sub.py
@@ -1,8 +1,4 @@
 import sys
 
 a, b = [int(i) for i in sys.stdin.readline().split(" ")]
-try:
-    c = (a+b)/(a-b) 
-    print(f"""{round(c, 2)}""")
-except Exception as e:
-    print(e)
+print(f"{round((a+b)/(a-b), 3):.2f}")
